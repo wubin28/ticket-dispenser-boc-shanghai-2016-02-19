75847feb3ba696facb9339e3ca4e92abd33a9f74
spider_263D048758 <spider@cyber-dojo.org>
2016-02-18 19:41:27 +0000

    1

---
+++ sandbox/TicketDispenserTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert.*;
+
+public class TicketDispenserTest {
+
+    @Test
+    public void should() {
+        assertEquals(1, 1);
+    }
+
+    //TODO  
+    //TODO 
+    //TODO 
+
+}
