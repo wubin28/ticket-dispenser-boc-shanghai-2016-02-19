2cc1e3378d6330d489adb87a6a5cfdefa8efb622
spider_263D048758 <spider@cyber-dojo.org>
2016-02-19 08:13:31 +0000

    47

--- sandbox/TurnNumberSequence.java
+++ sandbox/TurnNumberSequence.java
@@ -1,5 +1,13 @@
 public class TurnNumberSequence {
     private int turnNumber = 0;
+    
+    public TurnNumberSequence() {
+        this(0);
+    }
+
+    public TurnNumberSequence(int nextTurnNumber) {
+        this.turnNumber = nextTurnNumber;
+    }
 
     public int getNextTurnNumber()
     {
