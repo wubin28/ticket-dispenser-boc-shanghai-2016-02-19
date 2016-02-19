47bea109615eeab0698b4539a2f2d8aa72407fb7
spider_263D048758 <spider@cyber-dojo.org>
2016-02-19 06:21:27 +0000

    7

--- sandbox/TicketDispenserTest.java
+++ sandbox/TicketDispenserTest.java
@@ -8,9 +8,15 @@ public class TicketDispenserTest {
         assertEquals(1, 1);
     }
 
+    @Test
+    public void a_new_ticket_should_have_turn_number_subsequent_to_previous_one() {
+        assertEquals(1, newTicketTurnNumber - previousTicketTurnNumber);
+    }
+
     //TODO a_new_ticket_should_have_turn_number_subsequent_to_previous_one
     //TODO a_new_ticket_from_another_ticket_dispenser_should_have_turn_number_subsequent_to_previous_one
     //TODO a_new_ticket_should_have_the_given_turn_number
+
     //TODO vip_ticket_turn_numbers_should_begin_from_1001
     //TODO normal_ticket_turn_numbers_should_begin_from_2001
 
