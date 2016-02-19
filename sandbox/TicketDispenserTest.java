d5a2bacb91dfb9eea2a8011334528b6997a35f89
spider_263D048758 <spider@cyber-dojo.org>
2016-02-19 08:21:21 +0000

    50

--- sandbox/TicketDispenserTest.java
+++ sandbox/TicketDispenserTest.java
@@ -64,20 +64,18 @@ public class TicketDispenserTest {
 
         int firstVipTicketTurnNumber = vipTicketDispenser.getTurnTicket().getTurnNumber();
 
-        assertEquals(1001,firstVipTicketTurnNumber);
+        assertEquals(1001, firstVipTicketTurnNumber);
     }
     
     @Test
     public void normal_ticket_turn_numbers_should_begin_from_2001() {
-        TurnNumberSequence turnNumberSequence = new TurnNumberSequence(2001);
-        TicketDispenser normalTicketDispenser = new TicketDispenser(turnNumberSequence);    
+        TicketDispenser normalTicketDispenser = 
+            new TicketDispenser(new TurnNumberSequence(2001));    
 
-        int firstNormalTicketTurnNumber = normalTicketDispenser.getTurnTicket().getTurnNumber();
+        int firstNormalTicketTurnNumber =
+            normalTicketDispenser.getTurnTicket().getTurnNumber();
 
-        assertEquals(2001,firstNormalTicketTurnNumber);
+        assertEquals(2001, firstNormalTicketTurnNumber);
     }
-
-
-    //TODO normal_ticket_turn_numbers_should_begin_from_2001
 
 }
